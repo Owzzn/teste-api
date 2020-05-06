afb883fb15f145571d606a07feaa079cffa9c9a1
Vinicius Galli <vinicius.galli@hotmail.com>
2020-05-06 01:15:15 -0300

    Segunda mudança

--- server.js
+++ server.js
@@ -3,8 +3,4 @@ const express = require("express");
 const app = express();
 const port = 3010;
 
-app.get("/", (req, res) => {
-  res.send("mudei essa poha");
-});
-
 app.listen(port, () => console.log("Server is running on port" + port));
